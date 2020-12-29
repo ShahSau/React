ec3ec40dedfe23e4f18244c9adcf42a0f7f290c2
ShahSau <shahriarksaurov@gmail.com>
2020-12-29 11:05:57 +0200

    updated

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -5,12 +5,22 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state={
+    person:[
+      {name:'shah', age:90},
+      {name:'sah', age:60},
+      {name:'hah', age:50}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>hi, I am a React app</h1>
         <p>this is working</p>
-        <Person />
+        <button>Switch name</button>
+        <Person name={this.state.person[0].name}age={this.state.person[0].age}/>
+        <Person name={this.state.person[1].name}age={this.state.person[1].age}>hi, this is just a try.</Person>
+        <Person name={this.state.person[2].name}age={this.state.person[2].age}/>
       </div>
     );
   }
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 
-const person = ()=>{
-    return <p>i am a person!</p>
+const person = (props)=>{
+    return (
+    <div>
+        <p>i am a person name {props.name}! and i am {props.age} old</p>
+        <p>{props.children}</p>
+    </div>
+    )
 };
 
 export default person;

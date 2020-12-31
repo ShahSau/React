c7e7aa1eaedc928e53c6e2ede36cb15eb86bd885
ShahSau <shahriarksaurov@gmail.com>
2020-12-31 23:01:53 +0200

    updated

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -12,26 +12,76 @@ class App extends Component {
       {name:'hah', age:50}
     ]
   }
-  switchNameHandler= ()=>{
+  switchNameHandler= (newName,newAge)=>{
     this.setState({
       person:[
-        {name:'Shahriar', age:90},
+        {name:newName, age:90},
         {name:'sah', age:60},
-        {name:'hah', age:50}
+        {name:'hah', age:newAge}
       ]
     })
   }
+
+  nameChangedHandler = (event)=>{
+    this.setState({
+      person:[
+        {name:'newName', age:90},
+        {name:event.target.value, age:60},
+        {name:'hah', age:89}
+      ],
+      showPerson:false
+    })
+  }
+  togglePersonHandler=()=>{
+    this.setState((state) => {
+      if(state.showPerson === false){
+        return {showPerson: true}
+      }else{
+        return {showPerson:false}
+      }
+    })
+  }
   render() {
+    const style={
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding:'8px',
+      cursor: 'pointer'
+    }
+    let person=null
+    if(this.state.showPerson){
+      person=(
+      <div>
+        <Person 
+        name={this.state.person[0].name}
+        age={this.state.person[0].age}/>
+        <Person 
+        name={this.state.person[1].name}
+        age={this.state.person[1].age} click={this.switchNameHandler.bind(this,'maxi!!!!!',77)} changed={this.nameChangedHandler}>hi, this is just a try.</Person>
+        <Person 
+        name={this.state.person[2].name}
+        age={this.state.person[2].age}/>
+      </div>
+      )
+    }else{
+      person=(
+      <div>
+        <Person 
+        name={this.state.person[2].name}
+        age={this.state.person[2].age}/>
+      </div>
+      )
+    }
     return (
       <div className="App">
         <h1>hi, I am a React app</h1>
         <p>this is working</p>
-        <button onClick={this.switchNameHandler} >Switch name</button>
-        <Person name={this.state.person[0].name}age={this.state.person[0].age}/>
-        <Person name={this.state.person[1].name}age={this.state.person[1].age}>hi, this is just a try.</Person>
-        <Person name={this.state.person[2].name}age={this.state.person[2].age}/>
+        <button style={style}
+        onClick={this.togglePersonHandler} >Switch name</button>
+          {person}
       </div>
-    );
+    )
   }
 }
 
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import './Person.css'
 
 const person = (props)=>{
     return (
-    <div>
-        <p>i am a person name {props.name}! and i am {props.age} old</p>
+    <div className='Person'>
+        <p onClick={props.click}> i am a person name {props.name}! and i am {props.age} old</p>
         <p>{props.children}</p>
+        <p><input type="text" onChange={props.changed} value={props.name}/></p>
     </div>
     )
 };

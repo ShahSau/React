ee569d61bb5fd6b6f3027a5f0cb7e0fd47bacdd2
ShahSau <shahriarksaurov@gmail.com>
2021-01-07 14:51:02 +0200

    updated

--- Mini Projects/nasa_astronomy_pic/src/App.js
+++ Mini Projects/nasa_astronomy_pic/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route }  from 'react-router-dom'
 import Home from './components/Home'
 import NasaPhoto from './components/NasaPhoto'
 
+
 function App() {
   return (
    <BrowserRouter>

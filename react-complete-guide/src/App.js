e66e4373a169eafa79e6594a6707849c064e4860
ShahSau <shahriarksaurov@gmail.com>
2021-01-10 14:46:36 +0200

    updated

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -70,10 +70,19 @@ class App extends Component {
         border: '5px solid black',
       }
     }
+    //let classes=['red', 'bold'].join(' ')
+    let classes=[]
+    if(this.state.person.length<=2){
+      classes.push('red');
+    }
+    if(this.state.person.length<=1){
+      classes.push('bold')
+    }
+
     return (
       <div className="App">
         <h1>hi, I am a React app</h1>
-        <p>this is working</p>
+        <p className={classes.join(' ')}>this is working</p>
         <button style={style}
         onClick={this.togglePersonHandler} >Switch name</button>
           {person}
